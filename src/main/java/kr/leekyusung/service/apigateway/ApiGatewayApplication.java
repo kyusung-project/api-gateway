3ae1a26115fa633fcd2ade685419671071b1b4e0
LeeKyuSung <ks5050577@gmail.com>
2023-09-18 20:43:21 +0900

    api gateway 최종 세팅
    Update application.yml
    
    Create EurekaConfiguration.java
    
    Update application.yml

--- src/main/java/kr/leekyusung/service/apigateway/ApiGatewayApplication.java
+++ src/main/java/kr/leekyusung/service/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,12 @@ package kr.leekyusung.service.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +16,18 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        config.setNonSecurePort(8080);
+        return config;
+    }
 }
